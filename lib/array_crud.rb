139ae672c1bc31e6bb1cb7ad5d46ade7d87fec3a
Paul Ralyea <047par@gmail.com>
2020-03-06 19:24:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,15 @@
-def create_an_empty_array
-  
+def create_an_empty_array 
+  []
 end
-
-def create_an_array
   
+def create_an_array
+  [1, 2, 3, 4]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = ["lil' bub", "grumpy cat", "Maru"]
+  element = "nala cat"
+	famous_cats << "nala cat"
 end
 
 def add_element_to_start_of_array(array, element)
