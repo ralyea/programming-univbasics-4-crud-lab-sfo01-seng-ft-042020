ecda4179aec3e4f6d4bd4b4215502da75aff8a5f
Paul Ralyea <047par@gmail.com>
2020-03-06 19:31:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,7 @@ end
 def add_element_to_end_of_array(array, element)
   array = ["lil' bub", "grumpy cat", "Maru"]
   element = "nala cat"
-	famous_cats << "nala cat"
+	array << "nala cat"
 end
 
 def add_element_to_start_of_array(array, element)
